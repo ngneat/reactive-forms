265bdd73c9f9cefc37e176e93d1a4b30c704a901
Dan Roujinsky <danr@siemplify.co>
2020-05-24 09:53:03 +0300

    test(builder): improve tests

--- projects/ngneat/reactive-forms/src/lib/formBuilder.spec.ts
+++ projects/ngneat/reactive-forms/src/lib/formBuilder.spec.ts
@@ -15,26 +15,97 @@ describe('FormBuilder', () => {
   const fb = new FormBuilder();
 
   it('should group', () => {
-    const group: FormGroup<User> = fb.group<User>({ name: 'Dan', id: 1, address: { city: 'Hello' } });
-    expect(group.getRawValue()).toEqual({ name: 'Dan', id: 1, address: { city: 'Hello' } });
-    expect(group.getControl).toBeDefined();
+    const group: FormGroup<User> = fb.group({ name: 'ngneat', id: 1, address: { city: 'Hello' } });
+    expect(group.getRawValue()).toEqual({ name: 'ngneat', id: 1, address: { city: 'Hello' } });
+
+    const keys: (keyof FormGroup)[] = [
+      'getControl',
+      'enabledWhile',
+      'disabledWhile',
+      'touchChanges$',
+      'dirtyChanges$',
+      'valueChanges$',
+      'disabledChanges$',
+      'enabledChanges$',
+      'statusChanges$',
+      'errorChanges$',
+      'select',
+      'disabledWhile',
+      'enabledWhile',
+      'mergeValidators',
+      'mergeAsyncValidators',
+      'markAllAsDirty',
+      'validateOn',
+      'hasErrorAndTouched',
+      'hasErrorAndDirty',
+      'setEnable',
+      'setDisable'
+    ];
+
+    keys.forEach(key => expect(group[key]).toBeDefined());
   });
 
   it('should control', () => {
-    const control: FormControl<string> = fb.control('Hello');
-    expect(control.value).toEqual('Hello');
-    expect(control.enabledWhile).toBeDefined();
+    const control = fb.control('ngneat');
+    expect(control.value).toEqual('ngneat');
+
+    const keys: (keyof FormControl)[] = [
+      'enabledWhile',
+      'disabledWhile',
+      'touchChanges$',
+      'dirtyChanges$',
+      'valueChanges$',
+      'disabledChanges$',
+      'enabledChanges$',
+      'statusChanges$',
+      'errorChanges$',
+      'select',
+      'disabledWhile',
+      'enabledWhile',
+      'mergeValidators',
+      'mergeAsyncValidators',
+      'markAllAsDirty',
+      'validateOn',
+      'hasErrorAndTouched',
+      'hasErrorAndDirty',
+      'setEnable',
+      'setDisable'
+    ];
+    keys.forEach(key => expect(control[key]).toBeDefined());
   });
 
   it('should array', () => {
-    const array: FormArray<User> = fb.array<User>([fb.control({ name: 'Hello', id: 1, address: { city: 'Hello' } })]);
+    const array: FormArray<User> = fb.array([fb.control({ name: 'ngneat', id: 1, address: { city: 'ngneat' } })]);
     expect(array.getRawValue()).toEqual([
       {
-        name: 'Hello',
+        name: 'ngneat',
         id: 1,
-        address: { city: 'Hello' }
+        address: { city: 'ngneat' }
       }
     ]);
-    expect(array.markAllAsDirty).toBeDefined();
+
+    const keys: (keyof FormArray)[] = [
+      'enabledWhile',
+      'disabledWhile',
+      'touchChanges$',
+      'dirtyChanges$',
+      'valueChanges$',
+      'disabledChanges$',
+      'enabledChanges$',
+      'statusChanges$',
+      'errorChanges$',
+      'select',
+      'disabledWhile',
+      'enabledWhile',
+      'mergeValidators',
+      'mergeAsyncValidators',
+      'markAllAsDirty',
+      'validateOn',
+      'hasErrorAndTouched',
+      'hasErrorAndDirty',
+      'setEnable',
+      'setDisable'
+    ];
+    keys.forEach(key => expect(array[key]).toBeDefined());
   });
 });
