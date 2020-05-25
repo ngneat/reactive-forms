a20de18cba6d5c279e81553281ccdb1d01f2cdf6
Dan Roujinsky <danr@siemplify.co>
2020-05-25 11:30:26 +0300

    chore: add ControlValueAccessor and Validator to eslint

--- projects/ngneat/eslint-plugin-reactive-forms/lib/rules/no-angular-forms-imports.js
+++ projects/ngneat/eslint-plugin-reactive-forms/lib/rules/no-angular-forms-imports.js
@@ -27,7 +27,14 @@ module.exports = {
     // variables should be defined here
     const source = context.getSourceCode();
     const searchTerm = '@angular/forms';
-    const acceptedFormTypes = ['FormControl', 'FormArray', 'FormGroup', 'FormBuilder'];
+    const acceptedFormTypes = [
+      'FormControl',
+      'FormArray',
+      'FormGroup',
+      'FormBuilder',
+      'ControlValueAccessor',
+      'Validator'
+    ];
 
     //----------------------------------------------------------------------
     // Helpers
