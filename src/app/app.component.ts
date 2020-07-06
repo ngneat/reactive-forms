39c7e4d8a1e39c9d6cb9ae04a714662feaae3c4f
Netanel Basal <NetanelBasal@users.noreply.github.com>
2020-07-06 12:09:36 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,58 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@ngneat/reactive-forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@ngneat/reactive-forms';
+// import { FormGroup, FormControl } from '@angular/forms';
+
+interface Profile {
+  firstName: string;
+  lastName: string;
+  skills: string[];
+  controlArray: string[],
+  controlObject: {
+    key: string
+  },
+  address: {
+    street: string;
+    city: string;
+  };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms-playground';
-  @Input() formGroup: FormGroup;
+  profileForm = new FormGroup<Profile>({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    skills: new FormArray([]),
+    controlArray: new FormControl([]),
+    controlObject: new FormControl({value: { key: ''}}),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl('')
+    })
+  });
+
+  legacyForm = new FormGroup({
+    firstName: new FormControl(''),
+    skills: new FormArray([]),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl('')
+    })
+  });
 
   ngOnInit() {
-    let a = this.formGroup.controls.hello;
-    let b = this.formGroup.controls.hello.patchValue('');
+    this.profileForm.patchValue({firstName: 'Netanel'});
+  }
+
+  get skills() {
+    return this.profileForm.get('skills') as FormArray<string>;
+  }
+
+  addSkill() {
+    this.skills.push(new FormControl('JS'));
   }
 }
